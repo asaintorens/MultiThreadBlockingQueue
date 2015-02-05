c81a328dafca8ea04aa95da71ed0baf172f28421
asaintorens <alexandre.saintorens@gmail.com>
2015-02-05 17:49:47 +0100

    BlockingQueue
    BlockingQueue consommateur / producteur
    Edouard Souan + alex St-Orens

---
+++ MultiThreadTP4/ThreadSafe.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+public class ThreadSafe
+{
+    public static ConcurrentQueue<string> maListe = new ConcurrentQueue<string>();
+    static int limit = 200;
+    static int vitesseConsommateur = 800;
+    static int vitesseProducteur = 200;
+    public static Random random = new Random();
+    public static void Main()
+    {
+            new Thread(() => LancerProducteur()).Start();
+            new Thread(() =>LancerConsommateur()).Start();
+    }
+
+
+    public static void Producteur(int nb)
+    {
+        int produit = 0;
+        for (int compteurProduction = 0; compteurProduction < nb; compteurProduction++)
+        {
+            if (maListe.Count >= limit)
+            {
+
+            }
+            else
+            {
+                maListe.Enqueue(random.Next(100).ToString());
+                produit++;
+            }
+        }
+        Console.WriteLine("PRODUCTEUR : " + produit);
+
+    }
+
+    public static void Consommateur(int nb)
+    {
+        int consommé = 0;
+        for (int compteurConsommateur = 0; compteurConsommateur < nb; compteurConsommateur++)
+        {
+            string valeurDelete = "";
+            if (!maListe.IsEmpty)
+            {
+                consommé++;
+                maListe.TryDequeue(out valeurDelete);
+            }
+        }
+        Console.WriteLine("CONSOMME : " + consommé);
+    }
+
+    public static void LancerConsommateur()
+    {
+        while (true)
+        {
+            System.Threading.Thread.Sleep(vitesseConsommateur);
+            Consommateur(random.Next(100));
+        }
+    }
+    private static void LancerProducteur()
+    {
+        while (true)
+        {
+            Producteur(random.Next(100));
+            System.Threading.Thread.Sleep(vitesseProducteur);
+        }    
+    }
+
+
+}
---
+++ MultiThreadTP4V2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+public class ThreadSafe
+{
+    
+    static BlockingCollection<string> bloqueur;
+    static int limit = 30;
+    static int vitesseConsommateur = 5000;
+    static int vitesseProducteur = 300;
+    public static Random random = new Random();
+    public static void Main()
+    {
+        bloqueur = new BlockingCollection<string>(limit);
+        new Thread(() => LancerProducteur()).Start();
+        new Thread(() => LancerConsommateur()).Start();
+    }
+
+
+    public static void Producteur()
+    {
+
+   
+
+            try
+            {
+                bloqueur.Add("toto");
+                
+                Console.WriteLine("PRODUCTEUR : " + bloqueur.Count);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+                
+                 
+        
+
+        
+       
+
+    }
+
+    public static void Consommateur(int nb)
+    {
+        bool end = true;
+        for (int compteurConsommateur = 0; compteurConsommateur < nb; compteurConsommateur++)
+        {
+            string valeurDelete = "";
+            try
+            {
+
+                valeurDelete = bloqueur.Take();
+                
+                if (!end)
+                {
+                    Console.WriteLine("Out of stock");
+                    break;
+                }
+                Console.WriteLine("Nouveau consommateur : " + (compteurConsommateur + 1) + " / " + nb);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+      
+    }
+
+    public static void LancerConsommateur()
+    {
+        while (true)
+        {
+            System.Threading.Thread.Sleep(vitesseConsommateur);
+            Consommateur(random.Next(1,10));
+        }
+    }
+    private static void LancerProducteur()
+    {
+        while (true)
+        {
+            Producteur();
+            System.Threading.Thread.Sleep(vitesseProducteur);
+        }
+    }
+
+}
